97478c4ee9e4c7e618d9bf27c5fb6af267ff5e27
Safmaxser <md-86@mail.ru>
2024-11-16 16:54:13 +0700

    task completed dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const dropdownValue = dropdown.querySelector('.dropdown__value');
+  const dropdownList = dropdown.querySelector('.dropdown__list');
+  const dropdownItems = dropdown.querySelectorAll('.dropdown__item');
+  dropdownValue.addEventListener('click', () => {
+    const coordinates = dropdownValue.getBoundingClientRect();
+    dropdownList.style.top = coordinates.height + 'px';
+    dropdownList.style.left = coordinates.x + 'px';
+    dropdownList.classList.add('dropdown__list_active');
+  });
+
+  dropdownItems.forEach(dropdownItem => {
+    const dropdownLink = dropdownItem.querySelector('.dropdown__link');
+    dropdownLink.onclick = () => {
+      return false;
+    };
+    dropdownItem.addEventListener('click', () => {
+      dropdownValue.textContent = dropdownLink.textContent;
+     dropdownList.classList.remove('dropdown__list_active');
+    });
+  });
+});
