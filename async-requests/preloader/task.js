2e8fcbdeec5d87e8ce2f2aa73a3e8761a03e80a3
Safmaxser <md-86@mail.ru>
2024-12-06 12:31:57 +0700

    correction Preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,16 +1,35 @@
 const loader = document.getElementById('loader');
 const items = document.getElementById('items');
 const keyStorage = 'valuteList';
-const dataStorageObj = {};
+let dataStorageObj = {};
 const urlExchangeRate = 'https://students.netoservices.ru/nestjs-backend/slow-get-courses';
 let xhr = new XMLHttpRequest();
 xhr.responseType = 'json';
 xhr.timeout = 10000;
 
+window.addEventListener('load', () => {
+  xhr.open('GET', urlExchangeRate);
+  xhr.send();
+  const dataStorage = JSON.parse(localStorage.getItem(keyStorage));
+  if (dataStorage instanceof Object) { 
+    loader.classList.remove('loader_active');
+    const valuteListKey = Object.keys(dataStorage).sort();
+    valuteListKey.forEach(valuteKey => {
+      creatingPosition(valuteKey, dataStorage[valuteKey]);
+    });  
+  }
+});
+
 xhr.addEventListener('load', () => {
   if (xhr.readyState === xhr.DONE) {
-    if (xhr.status === 200) {
-      loader.classList.remove('loader_active');
+    if (xhr.status === 200) {      
+      loader.classList.remove('loader_active');      
+      const itemList = document.querySelectorAll('.item');
+      itemList.forEach(element => {
+        element.remove();
+      });
+      dataStorageObj = {};
+      localStorage.removeItem(keyStorage);
       const valuteObj = xhr.response.response.Valute;
       const valuteListKey = Object.keys(valuteObj).sort();
       valuteListKey.forEach(valuteKey => {
@@ -32,6 +51,10 @@ xhr.addEventListener('timeout', () => {
   xhr.open('GET', urlExchangeRate);
   xhr.send();
 });
+setInterval(() => {
+  xhr.open('GET', urlExchangeRate);
+  xhr.send();
+}, 600000);
 
 function creatingPosition(charCode, value) {
   dataStorageObj[charCode] = value;
@@ -49,17 +72,3 @@ function creatingPosition(charCode, value) {
     </div>
     `);
 };
-
-window.addEventListener('load', () => {
-  xhr.open('GET', urlExchangeRate);
-  xhr.send();
-  const dataStorage = JSON.parse(localStorage.getItem(keyStorage));
-  if (dataStorage instanceof Object) { 
-    loader.classList.remove('loader_active');
-    const valuteListKey = Object.keys(dataStorage).sort();
-    valuteListKey.forEach(valuteKey => {
-      creatingPosition(valuteKey, dataStorage[valuteKey]);
-    });  
-  }
-});
-
